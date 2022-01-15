1eb1225044a074d346c91624725119f4d926a7c9
YeeHHa <matthew.cradit@gmail.com>
2022-01-15 14:57:02 -0600

    pygame initiations

--- main_game.py
+++ main_game.py
@@ -2,9 +2,24 @@ import os
 import pygame
 import sys
 
+WIDTH, HEIGHT = 360, 480
+FPS = 60
+pygame.init() 
+screen = pygame.display.set_mode((WIDTH, HEIGHT)) 
+pygame.display.set_caption("martian game") 
+
+clock = pygame.time.Clock()
+
+while True:
+    clock.tick(FPS)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+           sys.exit()
 
 
-
+    screen.fill((0,0,0))
+    pygame.display.flip()
 
 
 
